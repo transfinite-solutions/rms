2440a7a8480eb8a012f5d131e0ba314eff19a6f3
Aayushsrivastav <aayushs2797@gmail.com>
2021-09-17 11:09:36 +0530

    Intial commit

---
+++ src/test/java/one/transfinite/rms/RmsApplicationTests.java
@@ -0,0 +1,13 @@
+package one.transfinite.rms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
